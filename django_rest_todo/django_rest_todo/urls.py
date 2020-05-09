9735b219a229047f5bc4431e6043f94a4353c43d
takeuchikatsuchi <k64katu195@gmail.com>
2020-05-09 16:39:23 +0900

    fromt-end-api

--- django_rest_todo/django_rest_todo/urls.py
+++ django_rest_todo/django_rest_todo/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from api import views
 
@@ -7,6 +8,6 @@ router.register(r'todos', views.TodoViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url('', RedirectView.as_view(url='/static/index.html')),
 ]
-
